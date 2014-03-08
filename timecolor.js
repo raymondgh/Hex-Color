e467ae99aaef602250193a5e91527b95b2d305eb
Ray Harris <raymondgh@gmail.com>
2014-03-07 19:28:50 -0500

    js file
    first upload

---
+++ timecolor.js
@@ -0,0 +1,78 @@
+var d = new Date();
+var hours=d.getHours();
+var startOfDay = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+var timesegment = (d.getTime()-Date.parse(startOfDay))/168750
+var timedbackground = grayscale(Math.floor(timesegment))
+
+
+function timeFunction()
+{
+	x=document.getElementById("time")          // Find the element
+	x.style.backgroundColor=timedbackground;          // Change the style
+	x.style.color=night(d.getHours());			// change style again
+}
+timeFunction();
+
+// Number -> Number
+// Takes a time block 0-511 and converts it to a color block 0-255
+
+function reblock(x)
+{
+	return Math.floor(255.5-Math.abs(x-255))+Math.floor(Math.round(x/511))
+}
+
+console.log(grayscale(0) == "000000");
+console.log(grayscale(87) == "575757");
+console.log(grayscale(255) == "ffffff");
+console.log(grayscale(256) == "ffffff");
+console.log(grayscale(511) == "000000");
+
+// Number -> string
+// Takes a number 0-255 and gives its corresponding grayscale hex code
+
+function grayscale(x)
+{
+	return hex1(x)+hex2(x)+hex1(x)+hex2(x)+hex1(x)+hex2(x)
+}
+
+// Number -> String
+// Takes a number 0-511 and gives first digit in hex code
+
+function hex1(x)
+{
+	return hexify(Math.floor(reblock(x)/16))
+}
+
+// Number -> String
+// Takes a number 0-511 and gives second digit in hex code
+
+function hex2(x)
+{
+	return hexify(Math.floor(reblock(x)%16))
+}
+
+// Number -> String
+// Takes a number 0-15 and gives a number 0-9 or letter a-f
+
+function hexify(x)
+{
+	var hexies = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+	return hexies.slice(x,x+1)
+}
+
+
+function night()  // determines ideal font color
+{
+if (hours>=6 && hours<=17)
+	{return "Black"}
+else
+	{return "White"}
+}
+/*
+document.getElementById("now").innerHTML=d.getTime();
+document.getElementById("nowhour").innerHTML=hours;
+document.getElementById("night").innerHTML=night(hours);
+document.getElementById("today").innerHTML=Date.parse(startOfDay);
+document.getElementById("thetime").innerHTML=d.getTime()-Date.parse(startOfDay);
+document.getElementById("colortime").innerHTML=Math.floor(timesegment);
+*/
