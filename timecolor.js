b8384ddedab5decde61da7983b5f943bdb1c7c4b
Ray Harris <raymondgh@gmail.com>
2014-03-10 23:13:20 -0400

    abstracted a bit

--- timecolor.js
+++ timecolor.js
@@ -2,15 +2,14 @@ var d = new Date();
 var hours=d.getHours();
 var startOfDay = new Date(d.getFullYear(), d.getMonth(), d.getDate());
 var timesegment = Math.floor((d.getTime()-Date.parse(startOfDay))/168750)
-var timedbackground = grayscale(timesegment)
 
 
-function timeFunction()
+
+function timeFunction() // Sets the background and text color 
 {
 	x=document.getElementById("time")          // Find the element
-	x.style.backgroundColor=timedbackground;          // Change the style
+	x.style.backgroundColor=grayscale(timesegment);          // Change the style
 	x.style.color=night(d.getHours());			// change style again
-	x.className=twilight();
 }
 timeFunction();
 
@@ -27,34 +26,19 @@ function reblock(x)
 
 function grayscale(x)
 {
-	return hex1(x)+hex2(x)+hex1(x)+hex2(x)+hex1(x)+hex2(x)
+	return hexify(reblock(x))+hexify(reblock(x))+hexify(reblock(x))
 }
 
 // Number -> String
-// Takes a number 0-511 and gives first digit in hex code
-
-function hex1(x)
-{
-	return hexify(Math.floor(reblock(x)/16))
-}
-
-// Number -> String
-// Takes a number 0-511 and gives second digit in hex code
-
-function hex2(x)
-{
-	return hexify(Math.floor(reblock(x)%16))
-}
-
-// Number -> String
-// Takes a number 0-15 and gives a number 0-9 or letter a-f
+// Takes a decimal 0-255 and produces its hexadecimal equivalent
 
 function hexify(x)
 {
-	var hexies = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
-	return hexies.slice(x,x+1)
+	var hexies = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
+	var firstDigit = Math.floor(reblock(x)/16)
+	var secondDigit = Math.floor(reblock(x)%16)
+	return hexies.slice(firstDigit,firstDigit+1) + hexies.slice(secondDigit,secondDigit+1);
 }
-
 
 function night()  // determines ideal font color
 {
@@ -67,7 +51,5 @@ else
 function twilight()  // determines ideal link style
 {
 if (hours<=2 || hours>=8 && hours<=15 || hours>=21)
-	{return 'extremestyle';}
-else
-	{return 'graystyle';}
+	{document.getElementById("time").className='extremestyle'}
 }
