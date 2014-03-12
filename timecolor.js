c8deaae45650adbd384eb07ef910d937f6f53478
Ray Harris <raymondgh@gmail.com>
2014-03-12 18:15:14 -0400

    hue function 99.6% operational

--- timecolor.js
+++ timecolor.js
@@ -10,48 +10,60 @@ function timeStyle() // Sets the background and text color
 
 	x.style.backgroundColor=grayscale(timesegment);
 
-	if (hours<=2 || hours>=8 && hours<=15 || hours>=21)
+	if (hours<=2 || ( hours>=8 && hours<=15 ) || hours>=21)
 	{x.className='extremestyle'}
+	else {x.className='graystyle'}
 
 	if (hours<=5 || hours>=18)
 	{x.style.color="white"}
+	else	{x.style.color="black"}
 }
 timeStyle();
-
-
-/* Ignore this code for now. Just brainstorming. 
 
 // String, integer -> String
 // Consumes a 6-digit hexadecimal color and a number, produces a hexidecimal color with an adjusted hue
 
 function hue(hex, x) 
 {
-	var r = hex.slice(0,2)
-	var g = hex.slice(2,4)
-	var b = hex.slice(4,6)
+	var r = decify(hex.slice(0,2))
+	var g = decify(hex.slice(2,4))
+	var b = decify(hex.slice(4,6))
 
-	if (x==0 || r==g && g==b) {return hex}; // other x values will return  hex as well, but varies by hex. can solve.
+	for (var i=x; i>0; i--)
+	{
+	if (x==0 || r==g && g==b)  // no adjustment or gray, return input
+		{
+		return hex  
+		} 
 
-	//loop through x here. essentially 4 remaining cases. need to study & condennse
-	if (r==b && r<g) {r++ i--}
-	if (r==g && r<b) {r++ i--}
-	if (r==b && r>g) {r-- i--}
-	if (r==g && r>b) {r-- i--}
+		if (x>0)
+			{
+			if (r>=g && r<b) {r++;} // increasing hue
+			if (b<=r && b>g) {b--;} // increasing hue
+			if (g>=b && g<r) {g++;} // increasing hue 
+			if (r<=g && r>b) {r--;} // increasing hue
+			if (b>=r && b<g) {b++;} // increasing hue
+			if (g<=b && g>r) {g--;} // increasing hue
+			}
 
-	if (g==b && g>r) {g-- i--}
-	if (g==r && g>b) {g-- i--}
-	if (g==b && g<r) {g++ i--}
-	if (g==r && g<b) {g++ i--}
-
-	if (b==g && b>r) {b-- i--}
-	if (b==r && b>g) {b-- i--}
-	if (b==g && b<r) {b++ i--}
-	if (b==r && b<g) {b++ i--}
-
-	if (none are the same) {reduce 2nd highest until 2 are the same}
-// need a way to reconsile positive and negative hue adjustments 
+		else 
+			{
+			if (g>=r && g<b) {g++;} // decreasing hue
+			if (b<=g && b>r) {b--;} // decreasing hue
+			if (r>=b && r<g) {r++;} // decreasing hue
+			if (g<=r && g>b) {g--;} // decreasing hue
+			if (b>=g && b<r) {b++;} // decreasing hue
+			if (r<=b && r>g) {r--;} // decreasing hue
+			}
+	}
+	return hexify(r)+hexify(g)+hexify(b)
 }
-*/
+
+console.log(hue("000000",0) == "000000");
+console.log(hue("0000ff",1) == "0100ff");
+console.log(hue("0200ff",2) == "0400ff");
+console.log(hue("0200ff",16) == "1200ff");
+
 
 
 // Number -> String
@@ -71,8 +83,8 @@ function grayscale(x)
 function hexify(x)
 {
 	var hexies = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"]
-	var firstDigit = Math.floor(reblock/16)
-	var secondDigit = Math.floor(reblock%16)
+	var firstDigit = Math.floor(x/16)
+	var secondDigit = Math.floor(x%16)
 	return hexies.slice(firstDigit,firstDigit+1) + hexies.slice(secondDigit,secondDigit+1);
 }
 
